a8f24da20b6011684d152e97d9b13333e0b8e8ae
urthbound <jeffowler@gmail.com>
2014-02-27 15:56:22 -0500

    four default vars

---
+++ scratch.clj
@@ -0,0 +1,48 @@
+(def x '(1 2 3 4 5 6 7))
+
+
+(defn anotherrev [alist]
+  (loop [thing alist
+         acc '()]
+   (if (= (count thing) 0)
+     acc
+     (recur (rest thing) (conj acc (first thing)))
+    )
+  )
+)
+
+(defn otherrev
+  ([a] (otherrev a '() ) )
+  ([thing acc]
+   (if (= (count thing) 0)
+     acc
+     (otherrev (rest thing) (conj acc (first thing)))
+    )
+  )
+)
+
+
+(defn rev_recur_to_fn [alist]
+  (let [tester (fn  [thing acc]
+   (if (= (count thing) 0)
+     acc
+     (recur (rest thing) (conj acc (first thing)))))]
+    (tester alist '() )
+    )
+)
+
+
+(defn rev_tail_call [alist]
+  (let [aaa (fn tester [thing acc]
+   (if (= (count thing) 0)
+     acc
+     (tester (rest thing) (conj acc (first thing)))))]
+    (tester alist '() )
+    )
+)
+
+(otherrev x)
+
+(anotherrev x)
+
+(therev x)
